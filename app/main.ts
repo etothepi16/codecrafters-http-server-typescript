e9f300a1eb8bae12673873c06f7949d2d1bb5c4d
Paul McDowell <paulmcdowell18@gmail.com>
2024-05-27 11:22:22 -0400

    extract url path

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,18 @@
 import * as net from "net"
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n")
-  socket.end()
+  socket.on("data", (data) => {
+    const reqComponents = data.toString().split("\r\n")
+    const requestLine = reqComponents[0].split(" ")
+    const requestTarget = requestLine[1]
+
+    if (requestTarget === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    }
+    socket.end()
+  })
 })
 
 server.listen(4221, "localhost", () => {
