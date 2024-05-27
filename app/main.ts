7fcad9bad35d79e9954126d0fda590bf8adcdeec
Paul McDowell <paulmcdowell18@gmail.com>
2024-05-27 13:27:43 -0400

    i don't even know

--- app/main.ts
+++ app/main.ts
@@ -170,7 +170,7 @@ const server = net.createServer((socket) => {
             "OK",
             {
               "Content-Type": "text/plain",
-              "Content-Length": String(compressed.length / 2),
+              "Content-Length": String(compressed.length),
               "Content-Encoding": "gzip",
             },
             compressed
